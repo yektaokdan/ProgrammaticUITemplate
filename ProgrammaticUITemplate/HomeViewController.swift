cf05a55afce5f45aba84fdb11f56feeaf12baaaa
yektaokdan <57546227+yektaokdan@users.noreply.github.com>
2024-03-05 09:07:47 +0300

    Initial commit

---
+++ ProgrammaticUITemplate/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  ProgrammaticUITemplate
+//
+//  Created by yekta on 5.03.2024.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
